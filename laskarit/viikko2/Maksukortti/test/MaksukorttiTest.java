333fac607ca69d21cfbd69e255be7c5d9f36f674
Elsa <nospam@nospam.com>
2019-03-26 21:23:58 +0200

    testit

--- laskarit/viikko2/Maksukortti/test/MaksukorttiTest.java
+++ laskarit/viikko2/Maksukortti/test/MaksukorttiTest.java
@@ -50,4 +50,33 @@ public class MaksukorttiTest {
 
         assertEquals("Kortilla on rahaa 10.0 euroa", vastaus);
     }
+       @Test
+    public void syoEdullisestiVahentaaSaldoaOikein() {
+        Maksukortti kortti = new Maksukortti(10);
+
+        kortti.syoEdullisesti();
+
+        assertEquals("Kortilla on rahaa 7.5 euroa", kortti.toString());
+    }
+    
+    @Test
+    public void syoMaukkaastiVahentaaSaldoaOikein() {
+        Maksukortti kortti = new Maksukortti(10);
+
+        kortti.syoMaukkaasti();
+
+        assertEquals("Kortilla on rahaa 6.0 euroa", kortti.toString());
+    }
+
+    @Test
+    public void syoEdullisestiEiVieSaldoaNegatiiviseksi() {
+        Maksukortti kortti = new Maksukortti(10);
+
+        kortti.syoMaukkaasti();
+        kortti.syoMaukkaasti();
+        // nyt kortin saldo on 2
+        kortti.syoEdullisesti();
+
+        assertEquals("Kortilla on rahaa 2.0 euroa", kortti.toString());
+    }  
 }
